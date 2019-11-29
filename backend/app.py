b9eedffb94da14977706dd5be6fd12086851bb3e
John Shin <jshin029@ucr.edu>
2019-11-29 10:31:19 -0800

    added twitter streaming and title parsing (#3)

--- backend/app.py
+++ backend/app.py
@@ -1,38 +1,5 @@
 from birdie import app
 from flask import Flask, request
-from config import *
-import tweepy
-
-auth = tweepy.OAuthHandler(api_key, api_secret_key)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
-@app.route('/', methods=['GET'])
-def index():
-  return 'Index'
-
-@app.route('/search', methods=['POST'])
-def search():
-  values = request.json
-  query, date, distance, hash_tags = [values[i] for i in ('query', 'date', 'distance', 'hashTags')]
-  result = {
-    'query': query,
-    'date': date,
-    'distance': distance,
-    'hash_tags': hash_tags
-  }
-  
-  return result
-
-@app.route('/getTweets', methods=['GET'])
-def getTweets():
-  tweets = []
-  public_tweets = api.home_timeline()
-  for tweet in public_tweets:
-      tweets.append(tweet._json)
-    
-  return { 'tweets': tweets }
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
