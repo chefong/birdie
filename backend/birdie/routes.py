fd96e20ac4183c876bc7c98e604d4120775525f3
John Shin <jshin029@ucr.edu>
2019-12-05 17:08:44 -0800

    Js reformat (#15)
    * added twitter streaming and title parsing
    
    * added routing and es insertion/indexing
    
    * input changes
    
    * switched from docker to bonsai
    
    * changes to insertion
    
    * reformat of tweet parsing
    
    * reformatted tweet stream
    
    * refomrat response

--- backend/birdie/routes.py
+++ backend/birdie/routes.py
@@ -2,33 +2,22 @@ from birdie import app
 from flask import jsonify, json, request
 from elasticsearch import Elasticsearch
 from math import radians, cos, sin, asin, sqrt # For coord radii
-from smallestenclosingcircle import make_circle
+from haversine import haversine, Unit
 
-# Haversine formula code referred heavily to: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-def coord_dist(long1, lat1, long2, lat2):
-    # Convert degrees to rad
-    long1, lat1, long2, lat2 = map(radians, [long1, lat1, long2, lat2])
+#import smallestenclosingcircle
 
-    # Haversine formula for great-circle distance
-    dist_long = long2 - long1
-    dist_lat = lat2 - lat1
-    a = sin(dist_lat / 2) ** 2 + cos(lat1) * sin(dist_long / 2) ** 2
-    c = 2 * asin(sqrt(a))
-    r = 3956 # Radius of Earth in miles
-    return c * r
-
-# Brute force method to calculate farthest points
-def farthest_points(points):
-    dist = 0
-    i = 0
-    for i < len(points) - 1:
-        j = i + 1
-        for j < len(points):
-            curr_dist = sqrt((points[i] - points[j])**2 + (points[i] - points[j])**2)
-            dist = max(dist, curr_dist)
-            j += 1
-        i += 1
-    return dist
+# # Brute force method to calculate farthest points
+# def farthest_points(points):
+#     dist = 0
+#     i = 0
+#     for i < len(points) - 1:
+#         j = i + 1
+#         for j < len(points):
+#             curr_dist = sqrt((points[i] - points[j])**2 + (points[i] - points[j])**2)
+#             dist = max(dist, curr_dist)
+#             j += 1
+#         i += 1
+#     return dist
 
 @app.route("/search", methods=["POST"])
 def query():
@@ -44,11 +33,13 @@ def query():
         longitude = data["longitude"]
         is_distance_restricted = data["isDistanceRestricted"]
         searchBy = data["searchBy"]
+        numResults = data["numResults"]
 
-        print("latitude: ", latitude)
-        print("longitude: ", longitude)
+        # print("latitude: ", latitude)
+        # print("longitude: ", longitude)
 
-        if searchBy == 'title':
+        if searchBy == 'Title':
+            print("searching by title")
             query = {
               "query": {
                 "bool" : {
@@ -58,7 +49,7 @@ def query():
                 }
               }
             }
-        
+
         else:
             query = {
               "query": {
@@ -70,7 +61,7 @@ def query():
               }
             }
 
-        res = es.search(index="tweetes", body=query)
+        res = es.search(index="tweet172", body=query, size=numResults)
         tweetObjects = res['hits']['hits']
         # List of pairs representing coordinates
         # Lat = Y, Long = X, per https://gis.stackexchange.com/a/68856
@@ -80,28 +71,30 @@ def query():
             if not is_distance_restricted:
               valid_tweets.append(tweet_json)
             elif len(tweet_json['_source']['coordinates']) > 0:
-                # Tweet coordinates come in as latitude, longitude
-                tweet_coord_lat = tweet_json['_source']['coordinates'][0]
-                tweet_coord_long = tweet_json['_source']['coordinates'][1]
+                #distance = coord_dist(tweet_coord_long, tweet_coord_lat, longitude, latitude)
+                input_list = [latitude, longitude]
 
-                distance = coord_dist(tweet_coord_long, tweet_coord_lat, longitude, latitude)
-                coords.append(tuple((tweet_coord_long, tweet_coord_lat)))
+                distance = haversine(tweet_json['_source']['coordinates'], input_list, unit=Unit.MILES)
+
+                coords.append(tweet_json['_source']['coordinates'])
 
                 if distance <= 100:
-                    print(tweet_json)
+                    # print(tweet_json)
                     print(distance)
                     valid_tweets.append(tweet_json)
 
-        circle = make_circle(coords)
-        center_x = circle[0]
-        center_y = circle[1]
-        # radius = circle[2]
-        valid_tweets["Center"] = [(9, 8)]
-
-        max_dist = farthest_points(coords)
-        valid_tweets["Max Dist"] = max_dist
-
-        return jsonify(valid_tweets)
+        # circle = make_circle(coords)
+        # center_x = circle[0]
+        # center_y = circle[1]
+        # # radius = circle[2]
+        # valid_tweets["Center"] = [(9, 8)]
+        #
+        # max_dist = farthest_points(coords)
+        # valid_tweets["Max Dist"] = max_dist
+        print(valid_tweets)
+        return jsonify({
+            "result": valid_tweets
+        })
     else:
         return "no input provided"
     return "no input provided"
